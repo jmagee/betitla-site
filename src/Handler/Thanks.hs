5f08e53953af8ef716d9bde2aa4744d356b37aa9
Joshua Magee <magee.joshua@yahoo.com>
2022-10-27 18:15:54 -0700

    Update thanks.

--- src/Handler/Thanks.hs
+++ src/Handler/Thanks.hs
@@ -9,11 +9,29 @@ module Handler.Thanks where
 
 import           Import
 
-import           Betitla.Striver (hasRequiredScope)
+import Betitla.Display
+import Betitla.Env
+import Betitla.Striver
+
+import Control.Monad.Reader (runReaderT)
+import Witch (from)
+
+--import           Betitla.Striver (hasRequiredScope)
+
+hasRequiredScope :: Text -> Bool
+hasRequiredScope text = isInfixOf "activity:read" text &&
+                        isInfixOf "activity:wrzte" text
+
+-- Fixme
+getAuthUrl' :: IO Text
+getAuthUrl' = getEnvRC >>= \env -> runReaderT getAuthUrl env >>= \case
+  Left e  -> pure $ "There was an error.  Blobfish says: " ++ display e
+  Right x -> pure $ from x
 
 getThanksR :: Handler Html
 getThanksR = defaultLayout $ do
   maybeScope  <- lookupGetParam "scope"
+  auth        <- liftIO (getAuthUrl' <&> (++ "&approval_prompt=force"))
   let scope   = maybe "No scope" id maybeScope
   let scopeOk = hasRequiredScope scope
   setTitle "Blobfish thanks you"
