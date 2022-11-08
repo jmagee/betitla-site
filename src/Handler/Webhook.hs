5a791639849ee9a0e2d31c24495f722b549f9fb2
Joshua Magee <magee.joshua@yahoo.com>
2022-11-07 20:02:59 -0800

    Add deauthorization handler.

--- src/Handler/Webhook.hs
+++ src/Handler/Webhook.hs
@@ -34,7 +34,7 @@ getWebhookR = do
     Just x  -> pure $ object ["hub.challenge" .= x]
 
 handleActivityEvent :: SubscriptionEvent -> Handler Bool
-handleActivityEvent  event =
+handleActivityEvent event =
   let activityId = ActivityId $ unsafeInto @Int64 (event ^. objectId)
       aspect     = event ^. aspectType
       athleteId  = event ^. ownerId
@@ -42,7 +42,26 @@ handleActivityEvent  event =
     $(logInfo) $ "Handling activity = " ++ tshow activityId ++
                  " with aspect = " ++ aspect ++
                  " and athelete = " ++ tshow athleteId
-    pure True
+    case aspect of
+      "create" -> pure True
+      x        -> $(logInfo) "We only rename on activity creations"
+               >> pure False
+
+handleAthleteEvent :: SubscriptionEvent -> Handler Bool
+handleAthleteEvent event =
+  let aspect    = event ^. aspectType
+      athleteId = AthleteId $ unsafeInto @Int64 (event ^. ownerId)
+  in do
+    $(logInfo) $ "Handling athlete = " ++ tshow athleteId ++
+                 " with aspect = " ++ aspect
+    case aspect of
+      x        -> pure False
+      "delete" -> do
+        env <- appEnv <$> getYesod
+        r <- liftIO $ runReaderT (removeUser athleteId) env
+        case r of
+          Left e  -> $(logError) (display e) >> pure False
+          Right _ -> $(logInfo) ("Removed " ++ tshow athleteId) >> pure True
 
 postWebhookR :: Handler Value
 postWebhookR = do
@@ -50,7 +69,7 @@ postWebhookR = do
   -- $(logInfo) $ from $ show postParams
   jsonBody    <- requireJsonBody :: Handler SubscriptionEvent
   $(logInfo) $ from $ show jsonBody
-  case (jsonBody ^. objectType) of
+  case jsonBody ^. objectType of
     "activity" -> do
       $(logInfo) "activity event"
       ok <- handleActivityEvent jsonBody
@@ -58,39 +77,9 @@ postWebhookR = do
 
     "athlete"  -> do
       $(logInfo) "athlete event"
+      ok <- handleAthleteEvent jsonBody
       pure $ object ["result" .= ("ok" :: Text)]
 
     x          -> do
       $(logWarn) $"unknown event, ignoring: " ++ x
       pure $ object ["result" .= ("ignore" :: Text)]
-  {-maybeType   <- lookupPostParam "object_type"-}
-  --maybeId     <- lookupPostParam "object_id"
-  {-maybeApsect <- lookupPostParam "aspect_type"-}
-  {-maybeOwner  <- lookupPostParam "owner_id"-}
-  {-$(logInfo) $ "Post webhook request received for " ++ from (show maybeOwner)-}
-  {-$(logInfo) $ "Post webhook request received for " ++ from (show maybeApsect)-}
-  {-$(logInfo) $ "Post webhook request received for " ++ from (show maybeId)-}
-  {-$(logInfo) $ "Post webhook request received for " ++ from (show maybeType)-}
-  --setTitle "Nothing to see here"
-  -- $(widgetFile "webhook")
-
-{-postHomeR :: Handler Html-}
-{-postHomeR = do-}
-    {-((result, formWidget), formEnctype) <- runFormPost sampleForm-}
-    {-let handlerName = "postHomeR" :: Text-}
-        {-submission = case result of-}
-            {-FormSuccess res -> Just res-}
-            {-_ -> Nothing-}
-
-    {-defaultLayout $ do-}
-        {-let (commentFormId, commentTextareaId, commentListId) = commentIds-}
-        {-aDomId <- newIdent-}
-        {-setTitle "Welcome To Yesod!"-}
-        {-$(widgetFile "homepage")-}
-{-getWebhookR :: Handler Html-}
-{-getWebhookR = defaultLayout $ do-}
-  {-maybeWord      <- lookupGetParam "hub.verify_token"-}
-  {-maybeHandshake <- lookupGetParam "hub.challenge"-}
-  {-let _ = Debug.trace ("word = " ++ show maybeWord ++ ", handshake =" ++ show maybeHandshake)-}
-  {-setTitle "Nothing to see here"-}
-  {-$(widgetFile "webhook")-}
